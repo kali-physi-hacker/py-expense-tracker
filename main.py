14ce08064fc88c68ac9bef57a395320a2735c3fe
Desmond Brown <browndesmond30@yahoo.com>
2023-05-10 16:25:03 +0000

    Initialized main.py file and implemented display_menu function

---
+++ main.py
@@ -0,0 +1,21 @@
+project_name = "Ky Expense Tracker"
+
+
+def display_menu():
+    print("1. Select your option")
+    print("2. View Expenses")
+    print("3. Update Expenses")
+    # ...
+
+
+def main():
+    underline = "======================================="
+    print(underline)
+    print(project_name)
+    print(underline)
+    display_menu()
+
+if __name__ == "__main__":
+    main()
+
+
